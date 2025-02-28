1fd8eef281d88a670f86830bfa33be52c92b7487
M509MMAO <marco.grecuccio@partenapromeris.be>
2025-02-28 16:55:16 +0100

    Adding newspaper app

---
+++ articles/views.py
@@ -0,0 +1,45 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+    CreateView,
+)
+from django.urls import reverse_lazy
+from articles.models import Article
+
+
+# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+
+class ArticleUpdateView(UpdateView):  # new
+    model = Article
+    fields = (
+        "title",
+        "body",
+    )
+    template_name = "article_edit.html"
+
+
+class ArticleDeleteView(DeleteView):  # new
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = (
+        "title",
+        "body",
+        "author"
+    )
--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     'accounts',
     'pages',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
